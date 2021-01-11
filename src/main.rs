39dbdfc144d6b9a354499e3dd5e97714b2509abc
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-01-11 09:29:51 +0100

    Refactor Fn millis_from_custom_epoch SystemTime argument

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,42 @@
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-pub fn timestamp_from_epoch(custom_epoch: u64) -> u64 {
-    let epoch = UNIX_EPOCH
-        .checked_add(Duration::from_millis(custom_epoch))
-        .expect("Error: Failed to create custom epoch.");
+pub fn millis_from_custom_epoch(custom_epoch: SystemTime) -> u64 {
     SystemTime::now()
-        .duration_since(epoch)
+        .duration_since(custom_epoch)
         .expect("Error: Failed to create Timestamp from custom epoch.")
         .as_millis() as u64
 }
 
 fn main() {
-    let millis_now = timestamp_from_epoch(0);
+    let millis_now = millis_from_custom_epoch(UNIX_EPOCH);
     println!("The current time in millis is {:#?}", millis_now)
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn timestamp_from_epoch() {
+    fn millis_from_epoch() {
         use super::*;
         use std::thread::sleep;
+        use std::time::Duration;
         let time_now = SystemTime::now();
         let millis_start = time_now
             .duration_since(UNIX_EPOCH)
             .expect("Error: Failed to get current time as duration from epoch.")
             .as_millis() as u64;
         sleep(Duration::from_millis(50));
-        // Test a CUSTOM EPOCH = UNIX EPOCH
-        let millis_after = timestamp_from_epoch(0);
+        // Test UNIX EPOCH
+        let millis_after = millis_from_custom_epoch(UNIX_EPOCH);
         assert!((millis_after - millis_start) < 52);
         // Test a CUSTOM EPOCH since launching test
+        let custom_epoch = UNIX_EPOCH
+            .checked_add(Duration::from_millis(millis_start))
+            .expect("Error: Failed to create custom epoch.");
         let millis_elapsed_time = time_now
             .elapsed()
             .expect("Error: Failed to get elapsed time.")
             .as_millis() as u64;
-        let millis_custom_epoch_time = timestamp_from_epoch(millis_start);
+        let millis_custom_epoch_time = millis_from_custom_epoch(custom_epoch);
         assert!((millis_elapsed_time - millis_custom_epoch_time) < 2);
     }
 }
