9c34154b0989c261b87baf95cb09b387aed4a938
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-01-12 17:21:57 +0100

    Finish function Next ID. Refactor input parameters

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,8 @@ struct Opt {
     sequence_bits: u8,
     #[structopt(default_value = "0", long)]
     node_id: u16,
+    #[structopt(default_value = "0", long)]
+    sign_bits: u8,
     #[structopt(default_value = ".env", long)]
     dotenv_file: String,
     #[structopt(long, short)]
@@ -67,15 +69,22 @@ impl SequenceProperties {
         node_id: u16,
         sequence_bits: u8,
         micros_ten_power: u8,
+        sign_bits: u8,
     ) -> Self {
-        let sign_bits = 1;
         let timestamp_bits = (64 as u8)
-            .checked_sub(sign_bits)
-            .unwrap()
-            .checked_sub(node_id_bits)
-            .unwrap()
             .checked_sub(sequence_bits)
-            .unwrap();
+            .expect(&format!(
+                "Error: Sequence bits is too large '{}'", sequence_bits))
+            .checked_sub(node_id_bits)
+            .expect(&format!(
+                "Error: Sum of bits is too large, maximum value 64. Node ID bits '{}', Sequence bits '{}'",
+                node_id_bits, sequence_bits
+            ))
+            .checked_sub(sign_bits)
+            .expect(&format!(
+                "Error: Sum of bits is too large, maximum value 64. Sign bits '{}', Sequence bits '{}', Node ID bits '{}'", 
+                sign_bits, sequence_bits, node_id_bits
+            ));
         SequenceProperties {
             custom_epoch,
             timestamp_bits,
@@ -98,15 +107,19 @@ pub fn generate_id(properties: &mut SequenceProperties) -> u64 {
         properties.custom_epoch,
         properties.micros_ten_power,
     ));
-    if properties.current_timestamp.unwrap() < properties.last_timestamp.unwrap() {
-        panic!(
-            "Error: Invalid System Clock state. New timestamp is earlier than previously registered timestamp."
-        )
+    if let Some(last_timestamp) = properties.last_timestamp {
+        if properties.current_timestamp.unwrap() < last_timestamp {
+            panic!(
+                    "Error: System Clock moved backwards. New timestamp is earlier than previously registered."
+                )
+        }
     }
     let new_id = to_id(properties);
     properties.sequence += 1;
     if properties.sequence == properties.max_sequence {
-        if properties.sequence == properties.max_sequence {}
+        if properties.sequence == properties.max_sequence {
+            // wait_next_timestamp()
+        }
     } else {
         // if timestamp changed reset to start a new sequence
         properties.sequence = 0;
@@ -114,11 +127,14 @@ pub fn generate_id(properties: &mut SequenceProperties) -> u64 {
     new_id
 }
 
+// pub fn wait_next_timestamp() {}
+
 pub fn to_id(properties: &mut SequenceProperties) -> u64 {
-    let mut id = properties.current_timestamp.unwrap()
-        << (properties.node_id_bits + properties.sequence_bits);
-    id |= (properties.node_id << properties.sequence_bits) as u64;
-    id |= properties.sequence_bits as u64;
+    let timestamp_shift_bits = properties.node_id_bits + properties.sequence_bits;
+    let node_id_shift_bits = properties.sequence_bits;
+    let mut id = properties.current_timestamp.unwrap() << (timestamp_shift_bits);
+    id |= (properties.node_id << node_id_shift_bits) as u64;
+    id |= properties.sequence as u64;
     id
 }
 
@@ -132,26 +148,32 @@ fn main() {
         ));
         for (key, value) in env::vars() {
             if key == "CUSTOM_EPOCH" && value != "" {
-                args.custom_epoch = value.clone();
-                println!("CUSTOM_EPOCH: {}", args.custom_epoch)
+                args.custom_epoch = value.parse::<String>().expect(&format!(
+                    "Error: Couldn't parse value CUSTOM_EPOCH '{}' as String, invalid UTF-8 characters", value)
+                );
             }
             if key == "NODE_ID_BITS" && value != "" {
-                args.node_id_bits = value.parse::<u8>().expect(
-                    "Error: NODE_ID_BITS couldn't be interpreted as value between 0 and 255",
-                );
-                println!("NODE_ID_BITS: {}", args.node_id_bits)
+                args.node_id_bits = value.parse::<u8>().expect(&format!(
+                    "Error: NODE_ID_BITS '{}' couldn't be interpreted as value between 0 and 255",
+                    value
+                ));
             }
             if key == "SEQUENCE_BITS" && value != "" {
-                args.sequence_bits = value.parse::<u8>().expect(
-                    "Error: SEQUENCE_BITS couldn't be interpreted as value between 0 and 255",
-                );
-                println!("SEQUENCE_BITS: {}", args.sequence_bits)
+                args.sequence_bits = value.parse::<u8>().expect(&format!(
+                    "Error: SEQUENCE_BITS '{}' couldn't be interpreted as value between 0 and 255",
+                    value
+                ));
             }
             if key == "MICROS_TEN_POWER" && value != "" {
-                args.micros_ten_power = value.parse::<u8>().expect(
-                    "Error: MICROS_TEN_POWER couldn't be interpreted as value between 0 and 255",
+                args.micros_ten_power = value.parse::<u8>().expect(&format!(
+                    "Error: MICROS_TEN_POWER '{}' couldn't be interpreted as value between 0 and 255", value)
                 );
-                println!("MICROS_TEN_POWER: {}", args.micros_ten_power)
+            }
+            if key == "SIGN_BITS" && value != "" {
+                args.sign_bits = value.parse::<u8>().expect(&format!(
+                    "Error: SIGN_BITS '{}' couldn't be interpreted as value between 0 and 255",
+                    value
+                ));
             }
         }
     }
@@ -162,22 +184,22 @@ fn main() {
         args.node_id,
         args.sequence_bits,
         args.micros_ten_power,
+        args.sign_bits,
     );
     let time_now = SystemTime::now();
-    generate_id(&mut properties);
-    println!(
-        "It took {:?} nanoseconds",
-        time_now
-            .elapsed()
-            .expect("Error: Failed to get elapsed time.")
-            .as_nanos()
-    );
+    let next_id = generate_id(&mut properties);
+    let elapsed = time_now
+        .elapsed()
+        .expect("Error: Failed to get elapsed time.")
+        .as_nanos();
+    println!("The id generated is: '{}'", next_id);
+    println!("It took {:?} nanoseconds", elapsed);
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn millis_from_epoch() {
+    fn timestamp_from_custom_epoch() {
         use super::*;
         use std::thread::sleep;
         use std::time::Duration;
@@ -189,43 +211,35 @@ mod tests {
         sleep(Duration::from_millis(50));
         // Test UNIX EPOCH
         let millis_after = timestamp_from_custom_epoch(UNIX_EPOCH, 3);
-        println!(
-            "millis_after {:?}, millis_start {:?}",
-            millis_after, millis_start
-        );
-        println!(
-            "millis_after - millis_start = {:?}",
-            millis_after.checked_sub(millis_start as u64)
-        );
         // 25ms more than expected 50ms as exponential distribution, CPU low-power
         // states and/or older hardware can cause signifficant differences.
         assert!((millis_after.checked_sub(millis_start as u64).unwrap()) < 75);
-        // Test a CUSTOM EPOCH since launching test
+        // Test a CUSTOM EPOCH in tenths of a millisecond
         let custom_epoch = UNIX_EPOCH
             .checked_add(Duration::from_millis(millis_start as u64))
             .expect("Error: Failed to create custom epoch.");
-        let millis_custom_epoch_time = timestamp_from_custom_epoch(custom_epoch, 3);
-        // Guarantee difference in millis
-        sleep(Duration::from_millis(1));
-        let millis_elapsed_time = time_now
+        let tenths_millis_custom_epoch_time = timestamp_from_custom_epoch(custom_epoch, 2);
+        // Wait a bit to prevent Option to call unwrap() on None below
+        // If both timestamps are within small margin substraction of u64
+        // can result in 'panicked at attempt to subtract with overflow'
+        // and checked_sub returns None value
+        sleep(Duration::from_millis(2));
+        // convert elapsed time from microseconds into tenths of a millisecond (0,1ms = 100 mcs)
+        let ten: u64 = 10;
+        let power_two: u32 = 2;
+        let tenths_millis_elapsed_time = (time_now
             .elapsed()
             .expect("Error: Failed to get elapsed time.")
-            .as_millis() as u64;
-        println!(
-            "millis_elapsed_time {:?}, millis_custom_epoch_time {:?}",
-            millis_elapsed_time, millis_custom_epoch_time
-        );
-        println!(
-            "millis_elapsed_time - millis_custom_epoch_time = {:?}",
-            millis_elapsed_time.checked_sub(millis_custom_epoch_time)
-        );
-        // 25ms more than expected 1ms as exponential distribution, CPU low-power
+            .as_micros() as u64)
+            / ten.pow(power_two);
+        // Substract custom epoch result with Rust's own elapsed time
+        // 150mcs more than expected 511mcs as exponential distribution, CPU low-power
         // states and/or older hardware can cause signifficant differences.
         assert!(
-            (millis_elapsed_time
-                .checked_sub(millis_custom_epoch_time)
+            (tenths_millis_elapsed_time
+                .checked_sub(tenths_millis_custom_epoch_time)
                 .unwrap())
-                < 26
+                < 150
         );
     }
 }
